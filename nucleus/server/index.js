1d128c3daf0af3b9d275f898388afa3241ced7c4
Phoenix <bootcampgithub@gmail.com>
2024-03-13 21:34:03 +0000

    set up server

--- nucleus/server/index.js
+++ nucleus/server/index.js
@@ -1,3 +1,9 @@
-import express from "express";
+import 'dotenv/config.js';
+import express from 'express';
 
 const app = express(); 
+
+const PORT = process.env.PORT || 3003;
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`)
+})
