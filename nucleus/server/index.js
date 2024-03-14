fb440f79f9d0e62c199dbcc3ca69a9a1d9e2e1b2
Phoenix <bootcampgithub@gmail.com>
2024-03-14 15:34:04 +0000

    connect server to database

--- nucleus/server/index.js
+++ nucleus/server/index.js
@@ -1,7 +1,21 @@
 import 'dotenv/config.js';
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express(); 
+
+app.use(cors(
+    // {
+    //     origin: ["FRONTEND"],
+    //     methods: ["POST", "GET", "PUT", "DELETE"],
+    //     credentials: true
+    // }
+));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, ()=>{
