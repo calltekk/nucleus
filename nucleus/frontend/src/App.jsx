c06ef757f0fde4577125a84ab2313d8f9597f692
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-03-14 19:37:17 +0000

    added all pages to App.jsx

--- nucleus/frontend/src/App.jsx
+++ nucleus/frontend/src/App.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { NavLink, Link } from "react-router-dom";
 
 import HomePage from "./pages/1-HomePage";
-import PomodoroTimer from "./components/timer";
-
+import TasksTimer from "./pages/2-Tasks-Timer";
+import TasksFullscreen from "./pages/3-Tasks";
+import TaskCreation from "./pages/4-Task-Creation";
 
 
 function App() {
   return (
     <>
       <HomePage />
+      <TasksTimer />
+      <TasksFullscreen />
+      <TaskCreation />
     </>
   )
 };
