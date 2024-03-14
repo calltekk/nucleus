27352e2251c5efca850ae05d5a2c7db560bb1bc7
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-03-14 19:16:31 +0000

    components-page hierarchy

--- nucleus/frontend/src/App.jsx
+++ nucleus/frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { NavLink, Link } from "react-router-dom";
-import TaskList from "./components/TaskList";
-import PomodoroTimer from "./components/timer";
+
+import HomePage from "./pages/1-HomePage";
 
 
 function App() {
   return (
     <>
-      <h1 className="border-2 p-5 text-5xl rounded-lg mx-auto my-10">Nucleus</h1>
-      <PomodoroTimer />
-      <TaskList />
+      <HomePage />
     </>
   )
 };
--- nucleus/frontend/src/pages/1-HomePage.jsx
+++ nucleus/frontend/src/pages/1-HomePage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import TaskList from "../components/TaskList";
+import Timer from "../components/timer";
 
 function HomePage() {
   	return (
-		<>
-			Homepage	
-		</>
+		<div id="home" className="min-h-dvh border-2 flex justify-evenly">
+			<TaskList />	
+			<Timer />
+		</div>
   	)
 };
 
