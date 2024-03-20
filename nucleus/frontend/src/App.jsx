b2c68a95fa0efc8d6b21a56a1a6c65363f94067f
Phoenix <bootcampgithub@gmail.com>
2024-03-20 03:08:53 +0000

    add routes

--- nucleus/frontend/src/App.jsx
+++ nucleus/frontend/src/App.jsx
@@ -1,20 +1,47 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import { NavLink, Link } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 import HomePage from "./pages/1-HomePage";
 import TasksTimer from "./pages/2-Tasks-Timer";
 import TasksFullscreen from "./pages/3-Tasks";
 import TaskCreation from "./pages/4-Task-Creation";
+import SignUp from "./pages/5-SignUp";
+import LogIn from "./pages/6-LogIn";
+import './assets/css/form.css';
+
+
 
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <>
-      <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route 
+          path="/"
+          element={<HomePage />}
+        />
+        <Route 
+          path="/signup"
+          element={!user ? <SignUp />: <Navigate to="/" />}
+        />
+        <Route 
+          path="/login"
+          element={ !user ? <LogIn />: <Navigate to="/" /> }
+        />
+      </Routes>
+    
+    </BrowserRouter>
+      
+      
       <TasksTimer />
       <TasksFullscreen />
       <TaskCreation />
+      
+
     </>
   )
 };
