f6f54ddd87909de2a77b2dc501ad75ec87841a82
Phoenix <bootcampgithub@gmail.com>
2024-03-20 09:06:22 +0000

    add log in page

---
+++ nucleus/frontend/src/pages/6-LogIn.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Header from "../components/Header";
+import { useLogin } from '../hooks/useLogIn'
+
+
+
+function LogIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const {login, error, isLoading} = useLogin();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await login(email, password);
+    }
+
+  	return (
+		<div>
+            <Header />
+            <form id="login" className="form" onSubmit={handleSubmit}>
+                <h1 className="form-title">Log In</h1>
+                 <input
+                    className="input-field"
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e)=> setEmail(e.target.value)}
+                    value={email}
+                />
+                 <input
+                    className="input-field"
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e)=> setPassword(e.target.value)}
+                    value={password}
+                />
+                <button className="submit-button" disabled={isLoading}>Log In</button>
+                {error && <div className="error">{error}</div>}
+            </form>
+        </div>
+  	)
+};
+
+export default LogIn;
