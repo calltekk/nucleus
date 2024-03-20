4c5095c01ca311bdf862fb19c8b6f73796b56be1
Phoenix <bootcampgithub@gmail.com>
2024-03-20 03:10:34 +0000

    add Login links

--- nucleus/frontend/src/components/Header.jsx
+++ nucleus/frontend/src/components/Header.jsx
@@ -1,17 +1,40 @@
-import React from "react";
-import logoImage from "../assets/images/nucleus-logo-white.svg";
+import React from 'react'
+import logoImage from '../assets/images/nucleus-logo-white.svg';
+import { NavLink, Link } from "react-router-dom";
+import { useLogout } from '../hooks/useLogOut';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 function Header() {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
-    <div className="bg-slate-300 bg-opacity-5 px-5 py-2 rounded-xl rounded-t-none sticky top-0 z-50 backdrop-blur-md">
-        <div className="navbar">
+    <div className="">
+        <div className="navbar bg-base-100">
         <div className="flex-1">
-            <a className="w-32 px-5 py-3 dark:hover:bg-slate-300 hover:bg-stone-900 dark:hover:bg-opacity-5 hover:bg-opacity-5 rounded-xl duration-500 cursor-pointer"><img id="logo" src={logoImage} alt="logo" fill="red" /></a>
+        <Link to="/"><span className="btn btn-ghost w-32"><img id="logo" src={logoImage} alt="logo" fill="red" /></span></Link>
         </div>
         <div className="flex-none">
-            <ul className="flex text-md px-5 gap-x-4">
-                <li><a className="px-5 py-3 dark:hover:bg-slate-300 hover:bg-stone-900 dark:hover:bg-opacity-5 hover:bg-opacity-5 rounded-lg duration-500 cursor-pointer">Settings</a></li> 
-                <li><a className="px-5 py-3 dark:hover:bg-slate-300 hover:bg-stone-900 dark:hover:bg-opacity-5 hover:bg-opacity-5 rounded-lg duration-500 cursor-pointer">Stats</a></li> 
+            <ul className="menu menu-horizontal px-1">
+                <li><a>Settings</a></li> 
+                {user && (
+                    <>
+                      <li><a>Stats</a></li>
+                      <li><button onClick={handleClick}>Log Out</button></li>
+                    </>
+                )}
+                
+                {!user && (
+                  <>
+                    <li><Link to="/login">Log In</Link></li>
+                    <li><Link to="/signup">Sign Up</Link></li>
+                  </>
+                )}
+                
             </ul>
         </div>
     </div>
