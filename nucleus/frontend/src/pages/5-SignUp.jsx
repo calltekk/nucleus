d4facc04db80c12b9d8b3352d9f42abe84db48a8
Phoenix <bootcampgithub@gmail.com>
2024-03-20 09:04:02 +0000

    add sign up page

---
+++ nucleus/frontend/src/pages/5-SignUp.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import {useSignup} from '../hooks/useSignUp';
+import Header from "../components/Header";
+
+function SignUp() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const {signup, error, isLoading} = useSignup();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await signup(name, email, password);
+        // console.log(email, password)
+    }
+
+  	return (
+		<div>
+            <Header />
+            <form id="signup" className="form" onSubmit={handleSubmit}>
+                <h1 className="form-title">Sign Up</h1>
+                <input
+                    className="input-field"
+                    type="text"
+                    placeholder="Name"
+                    onChange={(e)=> setName(e.target.value)}
+                    value={name}
+                />
+                 <input
+                    className="input-field"
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e)=> setEmail(e.target.value)}
+                    value={email}
+                />
+                 <input
+                    className="input-field"
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e)=> setPassword(e.target.value)}
+                    value={password}
+                />
+                <button className="submit-button" disabled={isLoading}>Sign Up</button>
+                {error && <div className="error">{error}</div>}
+            </form>
+        </div>
+  	)
+};
+
+export default SignUp;
